bd58b0823c30e1323a184143dcdac1cf6fd1a1e8
Quavke <132345908+Quavke@users.noreply.github.com>
2024-09-21 15:27:51 +0700

    Update app.sh

--- Docker/app.sh
+++ Docker/app.sh
@@ -2,12 +2,12 @@
 
 # Ожидание готовности базы данных
 until pg_isready -h db -p 1221 -U username; do
-  echo "Ожидание готовности базы данных..."
-  sleep 2
+    echo "Ожидание готовности базы данных..."
+    sleep 2
 done
 
 # Применение миграций Alembic
 alembic upgrade head
 
 # Запуск приложения
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000  --log-level app:app
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 app:app --log-level info
