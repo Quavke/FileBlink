afd94fb5330a928dde81081b3ec6ab2a405287bf
Quavke <132345908+Quavke@users.noreply.github.com>
2024-09-21 13:14:43 +0700

    Update app.sh

--- Docker/app.sh
+++ Docker/app.sh
@@ -10,4 +10,4 @@ done
 alembic upgrade head
 
 # Запуск приложения
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000  --log-level debug
